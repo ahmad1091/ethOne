226b0658c947f2e3012603fff770f0215c7ada1b
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-19 17:55:12 +0200

    costmize exported object

--- compile.js
+++ compile.js
@@ -5,4 +5,21 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
 const src = fs.readFileSync(inboxPath, "utf8");
 
-console.log(solc.compile(src, 1));
+var input = {
+  language: "Solidity",
+  sources: {
+    "inbox.sol": {
+      content: src,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  "inbox.sol"
+]["Inbox"];
